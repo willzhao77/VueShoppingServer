e0f8b98c4d35d13c2a1031267f4556883d0adccb
Will <willzhao77@gmail.com>
2020-03-05 22:21:07 +1300

    add user detail part. Use token update or create user details

--- app/Http/Controllers/Api/UserDetailsController.php
+++ app/Http/Controllers/Api/UserDetailsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\User;
+use App\UserDetails;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use DB;
@@ -70,6 +71,7 @@ class UserDetailsController extends Controller
     public function edit($id)
     {
         //
+        return "edit";
     }
 
     /**
@@ -79,9 +81,86 @@ class UserDetailsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $api_token)
     {
+
+      $user = User::where('api_token', hash('sha256', $api_token))->with('toDetails')->first();
+
+      // dd($user);
+
+      // return $user->id;
+      // $user->toDetails->user_id = $user->id;
+      // return 'ok';
+  // return $user->id;
+      if($user->toDetails != '')
+      {
+
+        $user->toDetails->name = $request->get('name');
+        $user->toDetails->address = $request->get('address');
+        $user->toDetails->mobile = $request->get('mobile');
+
+        if ($user->toDetails->save())
+        {
+          return "saved";
+        } else {
+          return "failed";
+        }
+      }else{
+
+          $userDetails = new UserDetails;
+
+          $userDetails->user_id = $user->id;
+          $userDetails->name = $request->get('name');
+          $userDetails->address = $request->get('address');
+          $userDetails->mobile = $request->get('mobile');
+          if ($userDetails->save())
+          {
+            return "saved";
+          } else {
+            return "failed";
+          }
+
+      }
+
+
+
+        // $this->validate($request, [
+        // 'name' => 'required',
+        // 'address' => 'required',
+        // 'mobile' => 'required',
+        // ]);
         //
+        // if($user->to_details){
+        //
+        // }else{
+        //   $user = new UserDetails;
+        //   $user->user_id = $id;
+        // }
+        //
+        //
+
+
+
+        //
+        // $user = UserDetails::where("user_id", $id)->first();
+        // if($user){
+        //
+        // }else{
+        //   $user = new UserDetails;
+        //   $user->user_id = $id;
+        // }
+        //
+        // $user->name = $request->get('name');
+        // $user->address = $request->get('address');
+        // $user->mobile = $request->get('mobile');
+        //
+        // if ($user->save())
+        // {
+        //   return "saved";
+        // } else {
+        //   return "failed";
+        // }
+
     }
 
     /**
